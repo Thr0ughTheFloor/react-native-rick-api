bb2d391cce2b68392649195d3543b2b5536db7e2
Fabio Luis Batista Santos <contatofabio2005@gmail.com>
2025-05-15 17:03:00 -0300

    Add files via upload

---
+++ app/_layout.tsx
@@ -0,0 +1,26 @@
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+
+
+
+export default function RootLayout() {
+
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  if (!loaded) {
+    // Async font loading only occurs in development.
+    return null;
+  }
+
+  return (
+    <>
+      <Stack>
+      <Stack.Screen name="index" options={{headerShown: false}}></Stack.Screen>
+      <Stack.Screen name="register" options={{headerShown: false}}></Stack.Screen>
+      <Stack.Screen name="(tabs)" options={{headerShown: false}}></Stack.Screen>
+      </Stack>
+    </>
+  );
+}
